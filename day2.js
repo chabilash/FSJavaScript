3293cd4ef4d822e659ddd14fb0993868f17fc545
abilashchintalacheruvula <chabilash@gmail.com>
2025-10-27 21:13:53 -0400

    day2 changes

---
+++ day2.js
@@ -0,0 +1,20 @@
+console.log("Abilash")
+
+fName = "Abi"
+console.log(fName)
+console.log(typeof(fName))
+
+rollNum = 501
+console.log(rollNum)
+console.log(typeof(rollNum))
+
+runStatus = true
+console.log(runStatus)
+console.log(typeof runStatus)
+
+/* lName;
+console.log(typeof lName) --> This gives Compilation error as - lName is not defined, not 'Undefined'*/
+
+flName = null
+console.log(flName)
+console.log(typeof flName) /* This gives object as datatype */
