5541adddca5966cb415b4277bb1d1c6c28b98fa3
abilashchintalacheruvula <chabilash@gmail.com>
2025-11-12 14:07:39 -0500

    day19-modules,export,import

---
+++ day19.js
@@ -0,0 +1,40 @@
+/*
+1. Modules
+2. this 
+3. Strings
+4. global Ogject
+5. Objects - Data Structures
+6. JSON
+*/
+
+
+/* //Common JS
+//---------
+function login(){
+    console.log("Login to Application!")
+}
+
+let fName = "Abilash"
+
+//This is the way of exporting in Common JS module - To export 1 or more items
+module.exports = {login , fName} */
+
+
+// ES Modules - This is in use from 2016 to till date
+//----------------------------------------------------
+
+function login(){
+    console.log("Login to Application!")
+}
+
+let fName = "Abilash"
+
+export {login , fName} // This si simple eway to export wharever we want
+//export default login; --> If we want to export only 1 item , we can usr this default
+
+
+
+
+
+
+
---
+++ day19_1.js
@@ -0,0 +1,28 @@
+
+/*  //Common JS
+ //--------------
+
+// const {fName , login} = require("./day19.js")
+//const {login} = require("./day19.js")  
+//const {login : myLogin} = require("./day19.js") // This is to to give alias name of login function while receiving
+//const {fName : myFName} = require("./day19.js")
+
+ console.log(fName)
+console.log(login)
+login() 
+
+console.log(myFName)
+console.log(myLogin)
+myLogin()  */
+
+
+// ES Modules import :
+import {login , fName} from "./day19.js"
+
+console.log(fName)
+console.log(login)
+login() 
+
+
+
+
