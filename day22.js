4eae6ea91a1485409721a04fd160ee8954e27f94
abilashchintalacheruvula <chabilash@gmail.com>
2025-11-15 10:39:56 -0500

    day22

---
+++ day22.js
@@ -0,0 +1,11 @@
+/*
+
+
+*/
+
+
+console.log("BEGIN")
+setTimeout(() => {console.log("setTimeout A Completed")}, 2100)
+setTimeout(() => {console.log("setTimeout B Completed")}, 2010)
+setTimeout(() => {console.log("setTimeout C Completed")}, 2001)
+console.log("END")
